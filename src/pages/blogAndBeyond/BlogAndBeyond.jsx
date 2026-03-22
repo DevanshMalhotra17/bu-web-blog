f0b866f67dd596ace5e746f5b85d0852dadf2938
DevanshMalhotra17 <devanshmalhotra17@gmail.com>
2026-03-22 00:09:53 -0400

    Refactor mobile layout to unified responsive design

--- src/pages/blogAndBeyond/BlogAndBeyond.jsx
+++ src/pages/blogAndBeyond/BlogAndBeyond.jsx
@@ -4,15 +4,10 @@ import Blog from "./Blog";
 import BlogExpanded from "./BlogExpanded";
 import { Header as HeaderDefault } from "./Header";
 import { Header as HeaderExpanded } from "./HeaderExpanded";
-import MobileBlog from "./mobile";
 import s from "./styles.module.css";
-import { IsMobile } from "./useIsMobile";
 
 export default function BlogAndBeyond() {
-  const isMobile = IsMobile();
   const [expandedId, setExpandedId] = useState(null);
-
-  if (isMobile) return <MobileBlog />;
 
   const HeaderComponent = expandedId
     ? () => <HeaderExpanded setExpandedId={setExpandedId} />
