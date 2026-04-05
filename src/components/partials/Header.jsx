76c93ad9674c37ab5686fbafb4cd5bcb3bb5823a
DevanshMalhotra17 <devanshmalhotra17@gmail.com>
2026-04-05 00:02:47 -0400

    feat: Redesign Header and Blog & Beyond landing page

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,8 +1,76 @@
+import { Link, useLocation } from "react-router-dom";
 import s from "./styles.module.css";
 
-// This is the 'default' orange header that is used across the project.
+const navItems = [
+  {
+    path: "/resources",
+    img: "/images/btn_01-resources.png",
+    alt: "Resources"
+  },
+  {
+    path: "/blog",
+    img: "/images/btn_02-blog.png",
+    alt: "Blog & Beyond"
+  },
+  {
+    path: "/lets-talk",
+    img: "/images/btn_03-lets.png",
+    alt: "Let's Talk"
+  },
+  {
+    path: "/", // Or wherever Dollars goes
+    img: "/images/btn_04-dollars.png",
+    alt: "Dollars Thoughts"
+  },
+  {
+    path: "/contribute",
+    img: "/images/btn_05-contrib.png",
+    alt: "I want to Contribute"
+  },
+];
+
 const Header = () => {
-  return <header>Header</header>;
+  const location = useLocation();
+
+  return (
+    <header className={s.header}>
+      <div className={s.container}>
+        
+        {/* Logo Section */}
+        <Link to="/" className={s.logoLink}>
+          <img src="/images/weare-logo.png" alt="Building U" className={s.mainLogo} />
+        </Link>
+        
+        {/* Navigation Section */}
+        <nav className={s.navGroup}>
+          {navItems.map((item, index) => {
+            const isActive = location.pathname.startsWith(item.path) && item.path !== "/";
+            return (
+              <Link
+                key={index}
+                to={item.path}
+                className={`${s.navItem} ${isActive ? s.active : ""}`}
+              >
+                <img src={item.img} alt={item.alt} className={s.navIcon} />
+                {/* We rely on the images themselves if they contain the text, 
+                    or fall back to accessible text if needed. Based on the mockup, 
+                    the icons often have the text embedded. If not, we can add spans.
+                */}
+              </Link>
+            );
+          })}
+        </nav>
+        
+        {/* Sign In Button */}
+        <div className={s.signInWrapper}>
+          <Link to="/signin-signup">
+            <img src="/images/btn_06-signup.png" alt="Sign In Up" className={s.signInBtn} />
+          </Link>
+        </div>
+        
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/blogAndBeyond/BlogAndBeyond.jsx
+++ src/pages/blogAndBeyond/BlogAndBeyond.jsx
@@ -6,24 +6,81 @@ import { Header as HeaderDefault } from "./Header";
 import { Header as HeaderExpanded } from "./HeaderExpanded";
 import s from "./styles.module.css";
 
-export default function BlogAndBeyond() {
+const BlogAndBeyond = () => {
+  const [isExploring, setIsExploring] = useState(false);
   const [expandedId, setExpandedId] = useState(null);
 
-  const HeaderComponent = expandedId
-    ? () => <HeaderExpanded setExpandedId={setExpandedId} />
-    : HeaderDefault;
+  if (isExploring) {
+    const HeaderComponent = expandedId
+      ? () => <HeaderExpanded setExpandedId={setExpandedId} />
+      : HeaderDefault;
+
+    return (
+      <Layout Header={HeaderComponent}>
+        {expandedId ? (
+          <BlogExpanded expandedId={expandedId} setExpandedId={setExpandedId} />
+        ) : (
+          <Blog setIsExpanded={setExpandedId} />
+        )}
+      </Layout>
+    );
+  }
 
   return (
-    <Layout
-      className={s.container}
-      Header={HeaderComponent}
-      showFooter={true}
-    >
-      {expandedId ? (
-        <BlogExpanded expandedId={expandedId} setExpandedId={setExpandedId} />
-      ) : (
-        <Blog setIsExpanded={setExpandedId} />
-      )}
+    <Layout showFooter={false}>
+      <div className={`${s.container} ${s.splitLayout}`}>
+        {/* Left Side: Magenta */}
+        <div className={s.leftSide}>
+          <div className={s.contentWrapper}>
+            <h1 className={s.title}>Blog & Beyond</h1>
+            <hr className={s.divider} />
+            <div className={s.buttonsGroup}>
+              <button 
+                className={s.buttonWrap} 
+                onClick={() => setIsExploring(true)}
+              >
+                <img src="/images/btn_01-blog.png" alt="Blog" className={s.iconBubble} />
+                <span>Blog</span>
+              </button>
+              <a 
+                href="https://www.youtube.com/@building-u" 
+                target="_blank" 
+                rel="noreferrer" 
+                className={s.buttonWrap}
+              >
+                <img src="/images/btn_02-youtube.png" alt="YouTube" className={s.iconYoutube} />
+                <span>Youtube</span>
+              </a>
+              <a 
+                href="https://open.spotify.com/user/building-u" 
+                target="_blank" 
+                rel="noreferrer" 
+                className={s.buttonWrap}
+              >
+                <img src="/images/btn_03-spotify.png" alt="Spotify Playlist" className={s.iconSpotify} />
+                <span>Spotify Playlist</span>
+              </a>
+            </div>
+          </div>
+          
+          <div className={s.footerWrapper}>
+            <span className={s.poweredText}>Powered by</span>
+            {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+              <div key={i} className={s.logoCircle}>LOGO</div>
+            ))}
+          </div>
+        </div>
+
+        {/* Right Side: Lime Green */}
+        <div className={s.rightSide}>
+          <div className={s.infoBox}>
+            We must write stuff about blog and channels of communication
+          </div>
+          <img src="/images/goat.png" alt="Goat" className={s.goat} />
+        </div>
+      </div>
     </Layout>
   );
-}
+};
+
+export default BlogAndBeyond;
